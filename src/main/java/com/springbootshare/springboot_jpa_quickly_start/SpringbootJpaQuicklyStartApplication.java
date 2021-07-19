d16ca811d8475bebdf875d64e8bf3663fcd8bf1f
CHENFE2 <felicity.chen@cargosmart.com>
2021-07-19 23:35:17 +0800

    init project by spring.io

---
+++ src/main/java/com/springbootshare/springboot_jpa_quickly_start/SpringbootJpaQuicklyStartApplication.java
@@ -0,0 +1,13 @@
+package com.springbootshare.springboot_jpa_quickly_start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaQuicklyStartApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootJpaQuicklyStartApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/springbootshare/springboot_jpa_quickly_start/SpringbootJpaQuicklyStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootshare.springboot_jpa_quickly_start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaQuicklyStartApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
